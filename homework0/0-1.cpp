d068d76ee558474db7a68cc760b9cc281648ace1
tj-messi <2715978522@qq.com>
2024-09-04 11:41:29 +0800

    1

---
+++ homework0/0-1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+ 
+void solve()
+{
+    int n;
+    n=12;
+    double ans=0;
+    for(int i=1;i<=n;i++)
+    {
+        double temp;
+        cin>>temp;
+        ans+=temp;
+    }
+    cout<<"��"<<setiosflags(ios::fixed)<<setprecision(2)<<ans/(double)12<<endl;
+}
+ 
+int main()
+{
+    int t;
+    t=1;
+    //cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
