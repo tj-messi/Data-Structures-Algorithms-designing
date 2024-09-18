03f10f78f6f2f9b726ef2795725532428c523860
tj-messi <2715978522@qq.com>
2024-09-18 14:10:09 +0800

    Create 1-2.cpp

---
+++ homework1/1-2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+
+
+void solve()
+{
+    int a1[10000],a2[10000];
+    int m;
+    cin>>m;
+    for(int i=1;i<=m+m;i++)
+        cin>>a1[i];
+    int n;
+    cin>>n;
+    for(int i=1;i<=n+n;i++)
+        cin>>a2[i];
+    int e=
+}
+ 
+int main()
+{
+    int t;
+    t=1;
+    //cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
